a2c03a9b1b33edca75e8617de5248d9c18a6a82e
RACHID EL AISSAOUI <rachid-tcs2000@outlook.fr>
2021-06-21 17:46:23 +0100

    add

--- add-product.php
+++ add-product.php
@@ -1,30 +1,53 @@
-<?php include('header.php'); ?>
-
+<?php 
+    include('header.php');
+    require_once('config.php');
+?>
 <div style="min-height:calc(100vh - 115px);max-width:960px;margin: 0 auto;" class="p-5 d-flex justify-content-center align-content-center">
     <div class="row w-75 mx-auto">
         <div class="p-4 border w-75 mx-auto">
             <div class="d-flex justify-content-between">
                 <span class="fs-m">Ajouter un article</span>
             </div>
-            <form method="POST" class="form-group">
+            <form method="POST" class="form-group" enctype="multipart/form-data">
                 <label class="label-control">Designation:</label>
-                <input name="designation" placeholder="Designation" class="form-control">
+                <input required name="designation" placeholder="Designation" class="form-control">
                 <label class="label-control">Prix:</label>
-                <input name="prix" placeholder="prix" class="form-control">
-                <label class="label-control mt-2" for="category">Categorie</label>
-                <select class="form-control" id="category" name="category">
-                    <option value="informatique">informatique</option>
+                <input required name="prix" pattern="[0-9.]*" placeholder="prix" class="form-control">
+                <label class="label-control mt-2" for="categorie">Categorie</label>
+                <select class="form-control" id="categorie" name="categorie">
+                    <option value="informatique" selected>informatique</option>
                     <option value="video">Video</option>
                     <option value="photo">Photo</option>
                     <option value="divers">Divers</option>
                 </select>
                 <label class="label-control mt-2" for="image">Image</label>
-                <input name="image" id="image" type="file" accept="image/png image/jpg image/jpeg" class="form-control">
-                <input type="submit" class="btn btn-primary mt-2" value="Valider" />
+                <input required name="image" id="image" type="file" accept="image/png image/jpg image/jpeg" class="form-control">
+                <input name="submit" type="submit" class="btn btn-primary mt-2" value="Valider" />
             </form>
         </div>
     </div>
 </div>
 </div>
 
-<?php include('footer.php'); ?>
+<?php
+
+    if(isset($_POST['submit'])){
+        $target_dir = "images/product/";
+        $target_file = 'images/product/' .  basename($_FILES['image']['name']);
+        $designation=@$_POST['designation'];
+        $prix=@$_POST['prix'];
+        $categorie =@$_POST['categorie'];
+        move_uploaded_file($_FILES['image']['tmp_name'],$target_file);
+        $image = $target_file;
+        $aid = "";
+
+        $query = "INSERT INTO article VALUES ('$aid', '$designation', '$prix', '$categorie', '$image');";
+        $result = mysqli_query($id,$query);
+        if($query)
+            echo "Submitted successfully";
+        else
+            echo "An error occured";
+    }    
+
+    include('footer.php'); 
+?>
