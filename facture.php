bc06e2ebffce4bfce279f5dab1c44d99d31cb1b9
Imad_CH <Imad2chaichaa@gmail.com>
2021-06-22 00:37:13 +0100

    a

--- facture.php
+++ facture.php
@@ -1,5 +1,37 @@
-<?php include_once('header2.php'); ?>
+<?php 
+   session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+   <meta charset="UTF-8">
+   <title>R&I MAGAZINE</title>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!--enable mobile device-->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!--fontawesome css-->
+   <link rel="stylesheet" href="css/font-awesome.min.css?v=1.1">
+   <!--bootstrap css-->
+   <link rel="stylesheet" href="css/bootstrap.min.css?v=1.1">
+   <!--main css-->
+   <link rel="stylesheet" href="css/style.css?v=1.3">
+   <!--my custom css-->
+   <script src="js/functions.js"></script>
+</head>
+
+<body >
+   <header>
+      <!-- Static navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+               <img src="images/logo.png" alt="">
+            </a>
+         </div>
+      </nav>
+   </header>
     
 
 
